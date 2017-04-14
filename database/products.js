b368e6ac8af7c2a692049cfdeee6dc97d537e5bc
PiotrBerebecki <petebere@gmail.com>
2017-04-14 20:22:55 +0100

    Add ids

--- database/products.js
+++ database/products.js
@@ -1,13 +1,16 @@
 module.exports = {
   '1': {
+    id: 1,
     name: 'dvd',
     price: 12345
   },
   '2': {
+    id: 2,
     name: 'gym equipment',
     price: 53532532
   },
   '3': {
+    id: 3,
     name: 'gardening equipment',
     price: 52352523
   }
