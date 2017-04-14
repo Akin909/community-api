7424e022c99e3c783317167bd4304c1ecabd4cbf
Akin909 <akin.sowemimo@gmail.com>
2017-04-14 19:24:38 +0100

    Initial commit

---
+++ database/products.js
@@ -0,0 +1,14 @@
+module.exports = {
+  '1': {
+    name: 'dvd',
+    price: 12345
+  },
+  '2': {
+    name: 'gym equipment',
+    price: 53532532
+  },
+  '3': {
+    name: 'gardening equipment',
+    price: 52352523
+  }
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const url = require('url');
+const products = require('./database/products');
+
+const app = express();
+
+
+app.get('/products', (req, res) => {
+  const id = url.parse(req.url, true).query.id;
+  console.log('id', id);
+  
+  res.send(products[id]);
+});
+
+
+const port = process.env.PORT || 4000;
+
+
+app.listen(port, () => {
+  console.log(`API server running on port ${port}`);
+});
