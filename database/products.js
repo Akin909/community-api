99b34bdecd8fb0d88e228d4e143c5a73e6e2c0ea
PiotrBerebecki <petebere@gmail.com>
2017-04-18 11:59:45 +0100

    Add slug

--- database/products.js
+++ database/products.js
@@ -2,19 +2,19 @@ module.exports = {
   '1': {
     id: 1,
     name: 'dvd',
-    path: 'dvd',
-    price: 11
+    slug: 'dvd',
+    price: 11,
   },
   '2': {
     id: 2,
     name: 'gym equipment',
-    path: 'gym-equipment',
-    price: 22
+    slug: 'gym-equipment',
+    price: 22,
   },
   '3': {
     id: 3,
     name: 'gardening equipment',
-    path: 'gardening-equipment',
-    price: 33
-  }
+    slug: 'gardening-equipment',
+    price: 33,
+  },
 };
